3f241a2afc454f2fb42979da814050e3e12bd7c5
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-06-07 13:52:17 -0700

    BE task types

---
+++ backend/src/types/task-types.ts
@@ -0,0 +1,32 @@
+export type TaskType = {
+  id: number;                   
+  taskName: string;
+  taskDescription: string;
+  taskCreatorId: number;
+  taskColourId: number;
+  createdAt: string;
+};
+
+export type NewTaskType = {
+  taskName: string;
+  taskDescription: string;
+  taskCreatorId: number;
+  taskColourId?: number;
+};
+
+export type TaskCategoryType = {
+  id: number;
+  name: string;
+  taskIds: number[];
+};
+
+export type NewTaskCategoryType = {
+  name: string;
+  taskIds?: number[];
+};
+
+export type AllTasksDataType = {
+  tasks: Record<number, TaskType>; // `tasks` object keys expected to be taskIds
+  taskCategories: Record<number, TaskCategoryType>; // `taskCategory` object keys expected to be taskCategoryIds
+  taskCategoryIdsOrder: number[];
+};
